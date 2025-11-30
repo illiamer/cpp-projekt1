04afcc5a2ba4fb7bd4706583d10220f62368564f
Illia <illiapromer@gmail.com>
2025-11-30 16:36:08 +0100

    Dodano funkcję isPrime()

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
 using namespace std;
+
+int factorial(int n) {
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+    int wynik = 1;
+    for (int i = 1; i <= n; i++) wynik *= i;
+    return wynik;
+}
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
 
 int main() {
     int a, b;
@@ -13,7 +27,26 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Oblicz silnie" << endl;   
+        cout << "2. Czy liczba jest pierwsza" << endl;  
+
         cin >> wyjscie;
+
+        if (wyjscie == 1) {
+            int x;
+            cout << "Podaj liczbe: ";
+            cin >> x;
+            cout << "Silnia = " << factorial(x) << endl;
+        }
+        if (wyjscie == 2) {
+            int x;
+            cout << "Podaj liczbe: ";
+            cin >> x;
+            if (isPrime(x)) cout << "Liczba jest pierwsza";
+            else cout << "Liczba nie jest pierwsza";
+        }
+
     } while(wyjscie != 0);
+
     return 0;
 }
